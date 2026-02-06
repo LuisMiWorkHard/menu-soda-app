6baaacbd4847aca629935aa4f1c13ec0d924619b
Luis Valeriano <luisvaleriano1009@gmail.com>
2026-02-05 23:01:21 -0500

    modificacion MainActivity para la navegacion y el AppModule

--- app/src/main/java/com/fullwar/menuapp/AppModule.kt
+++ app/src/main/java/com/fullwar/menuapp/AppModule.kt
@@ -1,7 +1,11 @@
 package com.fullwar.menuapp
 
+import com.fullwar.menuapp.presentation.features.login.LoginViewModel
+import com.fullwar.menuapp.presentation.features.shared.SharedViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModelOf(::SharedViewModel)
+    viewModelOf(::LoginViewModel)
 }
