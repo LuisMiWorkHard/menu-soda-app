6cb89c20d9b1c18bdabb19c0109084ccd0ea20fc
Luis Miguel Valeriano Vega <lvaleriano@derrama.org.pe>
2026-03-31 17:07:16 -0500

    Colores de app

--- app/src/main/java/com/fullwar/menuapp/ui/theme/Color.kt
+++ app/src/main/java/com/fullwar/menuapp/ui/theme/Color.kt
@@ -16,4 +16,39 @@ val SodaGray = Color(0xFF757575)
 val SodaGrayLight = Color(0xFFF5F5F5)
 val SodaGrayMedium = Color(0xFFE0E0E0)
 val SodaBackground = Color(0xFFFFFFFF)
+
+// Paleta Dark Elegante
+/*val DarkGreyLogo = Color(0xFF1E1E1E) // Fondo principal inspirado en el logo
+val SurfaceDark = Color(0xFF2C2C2C)  // Superficies y tarjetas
+val AccentGrey = Color(0xFFBDBDBD)   // Gris claro para acentos y elementos secundarios
+val PureWhite = Color(0xFFFFFFFF)    // Texto principal y contrastes altos
+val SubtleDivider = Color(0x1FFFFFFF) // Divisores sutiles con opacidad
+*/
+
+// Paleta Gris Carbón (#4b554f)
+val SurfaceDark = Color(0xFF2C2C2C)
+val Charcoal = Color(0xFF4B554F)
+val AccentGrey = Color(0xFFBDBDBD)
+val SoftBone = Color(0xFFFBF9F7)
+val DeepCharcoal = Color(0xFF1A1C1B)
+val RichBlack = Color(0xFF121413)
+val MutedGray = Color(0xFFB1B2AF)
+
+// Light Mode Colors
+val LightPrimary = Charcoal
+val LightBackground = SoftBone
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color.White
+val LightOnBackground = DeepCharcoal
+val LightOnSurface = DeepCharcoal
+
+// Dark Mode Colors
+val DarkPrimary = AccentGrey
+val DarkBackground = RichBlack
+val DarkSurface = DeepCharcoal
+val DarkOnPrimary = Color.White
+val DarkOnBackground = SoftBone
+val DarkOnSurface = SoftBone
+
+//Both
 val WhatsAppGreen = Color(0xFF25D366)
